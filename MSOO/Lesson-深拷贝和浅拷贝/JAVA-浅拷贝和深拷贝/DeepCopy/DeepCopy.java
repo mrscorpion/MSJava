9c3626b71d261850bb37a003f3a5c5a9bb17af1c
ms <1515679729@qq.com>
2017-01-07 16:07:18 +0800

    ShallowCopy & DeepCopy （Java & Objective-C）

---
+++ MSOO/Lesson-深拷贝和浅拷贝/JAVA-浅拷贝和深拷贝/DeepCopy/DeepCopy.java
@@ -0,0 +1,62 @@
+// 深拷贝(1) DeepCopy 
+class Professor implements Cloneable {
+    String name;
+    int age;
+    
+    Professor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public Object clone() {
+        Object o = null;
+        try {
+            o = super.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e.toString());
+        }
+        return o;
+    }
+}
+
+class Student implements Cloneable {
+    String name;
+    int age;
+    Professor p;
+    
+    Student(String name, int age, Professor p) {
+        this.name = name;
+        this.age = age;
+        this.p = p;
+    }
+    
+    public Object clone() {
+        Student o = null;
+        try {
+            o = (Student) super.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e.toString());
+        }
+        o.p = (Professor) p.clone();
+        return o;
+    }
+}
+
+public class DeepCopy {
+    public static void main(String args[]) {
+        long t1 = System.currentTimeMillis();
+        Professor p = new Professor("wangwu", 50);
+        Student s1 = new Student("zhangsan", 18, p);
+        Student s2 = (Student) s1.clone();
+        s2.p.name = "lisi";
+        s2.p.age = 30;
+        System.out.println("name=" + s1.p.name + "," + "age=" + s1.p.age);// 学生1的教授不改变。
+        long t2 = System.currentTimeMillis();
+        System.out.println(t2-t1);
+    }
+}
+
+/*!打印信息
+ name=wangwu,age=50
+ 1
+*/
--- MSOO/Lesson-深拷贝和浅拷贝/深拷贝和浅拷贝/深拷贝和浅拷贝/ViewController.m
+++ MSOO/Lesson-深拷贝和浅拷贝/深拷贝和浅拷贝/深拷贝和浅拷贝/ViewController.m
@@ -4,26 +4,40 @@
 //
 //  Created by ms on 2017/1/7.
 //  Copyright © 2017年 mrscorpion. All rights reserved.
-//
+//  NSString中为什么用copy？
+/*
+一般我们将对象声明为NSString类型时，都不希望它改变，所以大多数情况下，使用copy，以免因可变字符串的修改导致一些非预期问题。
+*/
 
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSString *strongString;
+@property (nonatomic, copy) NSString *copString;
+@property (nonatomic, strong) NSMutableString *strongStringM;
+@property (nonatomic, copy) NSMutableString *copStringM;
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *str = @"mr";
+    self.strongString = str;
+    self.copString = str;
+    NSLog(@"str => %p, strong => %p, copy => %p", str, self.strongString, self.copString);
+    // str => 0x10fc42078, strong => 0x10fc42078, copy => 0x10fc42078
+    
+    NSMutableString *strM = [NSMutableString stringWithFormat:@"hh"];
+    self.strongStringM = strM;
+    self.copStringM = strM;
+    NSLog(@"strM => %p, strongM => %p, copyM => %p", strM, self.strongStringM, self.copStringM);
+    // strM => 0x600000268180, strongM => 0x600000268180, copyM => 0xa000000000068682
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-
 @end

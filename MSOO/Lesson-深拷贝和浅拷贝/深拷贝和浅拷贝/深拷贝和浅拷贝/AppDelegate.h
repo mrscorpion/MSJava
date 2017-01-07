ca3460980456b3bc3d5917ed0acb80100f07d154
ms <1515679729@qq.com>
2017-01-07 13:25:58 +0800

    oop

---
+++ MSOO/Lesson-深拷贝和浅拷贝/深拷贝和浅拷贝/深拷贝和浅拷贝/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  深拷贝和浅拷贝
+//
+//  Created by ms on 2017/1/7.
+//  Copyright © 2017年 mrscorpion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MSOO/Lesson-深拷贝和浅拷贝/深拷贝和浅拷贝/深拷贝和浅拷贝/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  深拷贝和浅拷贝
+//
+//  Created by ms on 2017/1/7.
+//  Copyright © 2017年 mrscorpion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
